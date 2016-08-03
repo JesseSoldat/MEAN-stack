596fa50be896e0e4a3bf9bbc6faa82a44dfffac1
jesse soldat <jsoldat@hotmail.com>
2016-08-03 19:24:49 +0700

    jade works

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 var express = require('express'),
+    bodyParser = require('body-parser'),
 	stylus = require('stylus');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
 
 
 
 app.get('*', function(req, res) {
-	console.log(res);
+	res.render('index');
 });
 
 var port = 3000;
