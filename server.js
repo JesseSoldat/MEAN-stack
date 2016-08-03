b5f8f558c8512a0ffe26a8cabb1f7705062f2f75
jesse soldat <jsoldat@hotmail.com>
2016-08-03 19:42:50 +0700

    added stylus and all the scripts through bower

--- server.js
+++ server.js
@@ -6,11 +6,22 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
+function compile(str, path) {
+    return stylus(str).set('filename', path);
+}
+
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(stylus.middleware(
+{
+  src: __dirname + '/public',
+  compile: compile
+}
+));
 
 app.use(express.static(__dirname + '/public'));
 
