4b3e46e22945ffff1b93848c4a66425cfe3f2ba7
jesse soldat <jsoldat@hotmail.com>
2016-08-04 18:12:38 +0700

    got mongodb to pass a message to the frontend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
-	stylus = require('stylus');
+	stylus = require('stylus')
+	mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -25,12 +26,32 @@ app.use(stylus.middleware(
 
 app.use(express.static(__dirname + '/public'));
 
+//Mongoose
+ mongoose.connect('mongodb://jesse:ichiban987@ds011933.mlab.com:11933/jlab_parse');
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error...'));
+  db.once('open', function callback() {
+    console.log('JLab db opened');
+  });
+
+var messageSchema = mongoose.Schema({message: String}); 
+
+var Message = mongoose.model('Message', messageSchema);
+
+var mongoMessage;
+
+Message.findOne({}).exec(function(err, messageDoc){
+	// Message.create({message: ' Hello from MongoLab'});
+	mongoMessage = messageDoc.message;
+	console.log(mongoMessage);
+});
+
 app.get('/partials/*', function(req, res) {
     res.render('../../public/app/' + req.params[0]);
   });
 
 app.get('*', function(req, res) {
-	res.render('index');
+	res.render('index', {mongoMessage: mongoMessage});
 });
 
 var port = 3000;
